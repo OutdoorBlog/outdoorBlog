0491360460a639b324f3a3937ede2e0cd859822f
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2023-07-10 14:22:41 +0900

    엔티티 구성하기

--- springboot-developer/src/main/java/com/example/springbootdeveloper/SpringbootDeveloperApplication.java
+++ springboot-developer/src/main/java/com/example/springbootdeveloper/SpringbootDeveloperApplication.java
@@ -3,11 +3,13 @@ package com.example.springbootdeveloper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.boot.SpringApplication.*;
+
 @SpringBootApplication
 public class SpringbootDeveloperApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootDeveloperApplication.class, args);
+        run(SpringbootDeveloperApplication.class, args);
     }
 
 }
